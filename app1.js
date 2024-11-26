273f5f6f1c1053ce63211b1511a6a8826bc1f6d5
shravani <annamanenishravani01@gmail.com>
2024-11-26 15:20:37 +0530

    both dockerfile and nodejs file

---
+++ app1.js
@@ -0,0 +1,15 @@
+// Node.js program to display the multiplication table of 5
+
+// Define the number for which we need the table
+const number = 5;
+
+// Loop to display the table
+console.log(`Multiplication Table for ${number}:`);
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+app.listen( 2111, ( ) {
+console. log(server is running on port number  211ll")
+});
+
